1d4724f6a8e9b4c608ec48b7fd00429d65a0569f
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-10-28 09:11:56 +0300

    flutter shop ui

---
+++ lib/core.dart
@@ -0,0 +1,6 @@
+export 'package:shopping_ui/module/cart_screen.dart';
+export 'package:shopping_ui/module/detail_product_screen.dart';
+export 'package:shopping_ui/module/home_screen.dart';
+export 'package:shopping_ui/module/register_screen.dart';
+export 'package:shopping_ui/module/welcome_screen.dart';
+export 'package:shopping_ui/services/services_grocery.dart';
